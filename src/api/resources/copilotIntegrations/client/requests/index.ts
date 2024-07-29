43a3f3cc1a9d399cc06a95e3c8df81267bbe7f46
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2024-07-29 03:37:22 +0000

    Release 0.0.1-beta0

---
+++ src/api/resources/copilotIntegrations/client/requests/index.ts
@@ -0,0 +1,1 @@
+export { type CreateStreamRequest } from "./CreateStreamRequest";
---
+++ src/serialization/resources/index.ts
@@ -0,0 +1,5 @@
+export * as copilotIntegrations from "./copilotIntegrations";
+export * from "./copilotIntegrations/types";
+export * from "./copilotIntegrations/client/requests";
+export * as misc from "./misc";
+export * from "./misc/client/requests";
